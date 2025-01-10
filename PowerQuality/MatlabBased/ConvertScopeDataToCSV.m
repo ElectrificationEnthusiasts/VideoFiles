93dcb2820a4fe2ae63324f5d45b3f15d4aa6b154
Joel Van Sickel <jvansick@mathworks.com>
2025-01-10 09:17:21 -0500

    power quality work: updating model to work on only a window of the simulink model to make results more obvious

--- PowerQuality/MatlabBased/ConvertScopeDataToCSV.m
+++ PowerQuality/MatlabBased/ConvertScopeDataToCSV.m
@@ -4,10 +4,20 @@
 time = PowerQualityData{1}.Values.Time;
 
 names = {};
-T = table(time);
+
+
+tstart = 0.25;
+tstop = 0.32;
+dt = time(2)-time(1);
+ind1 = find(time == tstart);
+ind2 = find(time == tstop);
+n = ind2-ind1;
+tnew = 0:dt:(dt*n);
+T = table(tnew');
 for i = 1:PowerQualityData.numElements
     name = PowerQualityData{i}.Values.Name;
     data = PowerQualityData{i}.Values.Data;
+    data = data(ind1:ind2);
     T = addvars(T,data,'NewVariableNames',name);
 end
 writetable(T,'power_quality_data.csv')
